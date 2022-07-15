58b64ac73bc513b7c145013ace08c3ec6ff744bf
unknown <megancaber7@gmail.com>
2022-07-15 12:43:43 -0700

    update webservices

--- src/services/webservices.ts
+++ src/services/webservices.ts
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+// export function getAppURL() {
+//   return (
+//     "http://localhost:8080/api"
+//   )
+// }
 export function getAppURL() {
-  return (
-    "http://localhost:8080/api"
-  )
+  // console.log('location: ' + window.location.href);
+   if (window.location.href.toLowerCase().indexOf('localhost') > -1) {
+       return '/api/WS/'//can do this with proxy set in package.json
+   }
+   else {
+       return '../api/WS/';
+   }
 }
